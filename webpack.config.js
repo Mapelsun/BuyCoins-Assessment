0c164883176cac72018871c3f6c25c3c597d6680
unknown <pelumisunday@gmail.com>
2020-11-21 20:14:00 +0100

    feat: complete webpack setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,90 @@
 const path = require("path");
-const webpack = require("webpack");
-const dotenv = require("dotenv").config({
-  path: path.join(__dirname, ".env"),
-});
+const Dotenv = require("dotenv-webpack");
+const htmlWebpackPlugin = require("html-webpack-plugin");
+// const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+// const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-  entry: "./src/app.js",
+  // entry: ["./src/js/index.js", "./src/assets/main.scss"],
+    entry: "./src/app.js",
+
   output: {
+    filename: "[name].[contenthash:8].js",
     path: path.resolve(__dirname, "public/scripts"),
-    filename: "bundle.js",
+    chunkFilename: "[name].[contenthash:8].js",
   },
+
   module: {
     rules: [
       {
-        test: /\.js?$/,
-        exclude: /(node_modules)/,
-        include: path.resolve(__dirname, "src"),
+        test: /\.js$/,
+        exclude: /node_modules/,
         use: {
           loader: "babel-loader",
         },
       },
+      // {
+      //   test: /\.s?css$/,
+      //   use: [
+      //     "style-loader",
+      //     MiniCssExtractPlugin.loader,
+      //     "css-loader",
+      //     {
+      //       loader: "postcss-loader",
+      //     },
+      //     "sass-loader",
+      //   ],
+      // },
+      // {
+      //   test: /\.(eot|ttf|woff|woff2)(\?\S*)?$/,
+      //   loader: "file-loader",
+      // },
+      // {
+      //   test: /\.(png|jpe?g|gif|webm|mp4|svg)$/,
+      //   loader: "file-loader",
+      //   options: {
+      //     name: "[name][contenthash:8].[ext]",
+      //     outputPath: "assets/img",
+      //     esModule: false,
+      //   },
+      // },
     ],
   },
+
   plugins: [
-    new webpack.DefinePlugin({
-      "process.env": dotenv.parsed,
+    // new CleanWebpackPlugin(),
+    // new MiniCssExtractPlugin({
+    //   filename: "[name].[contenthash:8].css",
+    //   chunkFilename: "[name].[contenthash:8].css",
+    // }),
+    new htmlWebpackPlugin({
+      template: path.resolve(__dirname, "public", "index.html"),
+      favicon: "./public/images/github-favicon.svg",
+    }),
+    new Dotenv({
+      path: "./.env",
+      safe: true,
     }),
   ],
+
+  // optimization: {
+  //   moduleIds: "deterministic",
+  //   runtimeChunk: "single",
+  //   splitChunks: {
+  //     cacheGroups: {
+  //       vendor: {
+  //         test: /[\\/]node_modules[\\/]/,
+  //         name: "vendors",
+  //         priority: -10,
+  //         chunks: "all",
+  //       },
+  //     },
+  //   },
+  // },
+
+  devServer: {
+    contentBase: path.resolve(__dirname, "public"),
+    historyApiFallback: true,
+  },
 };
+
